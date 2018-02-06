6a85f446d97dd96224d1902ed75c294ac28a10be
Gustav Persson <gustav.persson@modcam.io>
2018-02-06 19:01:26 +0100

    Updated text and design

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import _ from 'lodash'
 
 import {
-  Container, Divider, Header, Icon, Image, List, Menu, Segment, Visibility,
+  Container, Divider, Header, Icon, Image, List, Menu, Segment, Visibility, Responsive
 } from 'semantic-ui-react'
 
 import faceSmall from './assets/face_small.jpg';
@@ -70,7 +70,7 @@ class App extends Component {
 
 
   render() {
-    const { menuFixed, overlayFixed, overlayRect } = this.state
+    const { menuFixed, overlayFixed } = this.state
 
     return (
       <div>
@@ -95,6 +95,33 @@ class App extends Component {
 
         <Container text>
           <Image src={face} size='medium' centered />
+            <Container fluid textAlign='center'>
+
+          <Menu
+            compact
+            icon='labeled'
+            horizontal
+            style={{
+              margin: '0 auto'
+            }}
+          >
+            <Menu.Item as="a" href="https://twitter.com/gustavpersson">
+              <Icon name='twitter' />
+             Twitter
+            </Menu.Item>
+
+            <Menu.Item as="a" href="https://github.com/GustavPersson">
+              <Icon name='github' />
+             Github
+            </Menu.Item>
+
+            <Menu.Item as="a" href="mailto:gustav.e.persson@gmail.com" >
+              <Icon name='mail' />
+             Email
+            </Menu.Item>
+          </Menu>
+        </Container>
+          <Divider />
           <p>
             I am a software developer with good skills and a special
             interest in modern web technologies and games programming.
@@ -129,43 +156,13 @@ class App extends Component {
             supporter, I have a season pass on section 10, I enjoy watching sports, and spend
             whatever extra time I have with my girlfriend, our cats, and playing computer games.
           </p>
-          <Visibility
-            offset={80}
-            once={false}
-            onTopPassed={this.stickOverlay}
-            onTopVisible={this.unStickOverlay}
-            style={overlayFixed ? { ...overlayStyle, ...overlayRect } : {}}
-          />
 
-          <div
-            ref={this.handleOverlayRef}
-            style={overlayFixed ? fixedOverlayStyle : overlayStyle}
-          >
-            <Menu
-              icon='labeled'
-              style={overlayFixed ? fixedOverlayMenuStyle : overlayMenuStyle}
-              vertical
-            >
-              <Menu.Item as="a" href="https://twitter.com/gustavpersson">
-                <Icon name='twitter' />
-               Twitter
-              </Menu.Item>
-
-              <Menu.Item as="a" href="https://github.com/GustavPersson">
-                <Icon name='github' />
-               Github
-              </Menu.Item>
-
-              <Menu.Item as="a" href="mailto:gustav.e.persson@gmail.com" >
-                <Icon name='mail' />
-               Email
-              </Menu.Item>
-            </Menu>
-          </div>
-
-
-          <Header as='h1'>Experience</Header>
-          <Divider horizontal>ModCam AB - <span>Front End Lead - Malmö, Sweden - 2016 - </span></Divider>
+          <Header as='h1' textAlign='center'>Experience</Header>
+          <Divider horizontal>Modcam AB
+            <Responsive as={'span'} minWidth={768}>
+              {' - Front End Lead - Malmö, Sweden - 2016 -'}
+            </Responsive>
+          </Divider>
           <p>
             Worked as a Front End Lead for a Malmö based Startup working with business intelligence,
             developing and maintaining front end solutions for customers to view their data, and
@@ -180,7 +177,11 @@ class App extends Component {
             built around Webpack and Babel, compiling to both NodeJS-compatible code,
             and code that runs on the client.
           </p>
-          <Divider horizontal>Mobenga AB - <span>Front End Developer - Malmö, Sweden - 2015 - 2016 </span></Divider>
+          <Divider horizontal>Mobenga AB
+            <Responsive as={'span'} minWidth={768}>
+             {' - Front End Developer - Malmö, Sweden - 2015 - 2016'}
+           </Responsive>
+          </Divider>
           <p>
             Worked as a Front End Developer in several teams, developing Sportsbook applications.
             The applications ranged from web apps in a native wrapper, designed only for mobile
@@ -199,7 +200,11 @@ class App extends Component {
             JavaScript using the newest syntaxes.
           </p>
 
-          <Divider horizontal>HiQ Skåne AB - <span>Programmer - Lund, Sweden - 2013 - 2015</span></Divider>
+          <Divider horizontal>HiQ Skåne AB
+            <Responsive as={'span'} minWidth={768}>
+              {' - Consultant - Lund, Sweden - 2013 - 2015'}
+            </Responsive>
+         </Divider>
           <Header as="h4">Consultancy Assignments</Header>
           <List bulleted>
             <List.Item>
@@ -209,7 +214,11 @@ class App extends Component {
             <List.Item>Web App Frontend Developer, Blackberry Blend, Blackberry </List.Item>
             <List.Item>Web Frontend Developer, InfabVitamin AB </List.Item>
           </List>
-          <Divider horizontal>Sony Mobile AB - <span>App Programmer - Lund, Sweden - June 2013 - Nov 2013</span></Divider>
+          <Divider horizontal>Sony Mobile AB
+            <Responsive as={'span'} minWidth={768}>
+              {' - App Programmer - Lund, Sweden - June 2013 - Nov 2013'}
+            </Responsive>
+          </Divider>
           <p>
             Worked on Sony’s Firefox OS project, in a small team managing the core apps (e.g. communications, calendar, SMS and the lock screen).
             The team made bug fixes to, “Sonyfied”, and implemented new features to several of the apps, allowing for basic prototype phones to be created.
@@ -218,7 +227,12 @@ class App extends Component {
           <p>
             Was also part of the Platform team, working with making sure that the code base was regularly being kept up to date with Mozilla.
           </p>
-          <Divider horizontal><span>Platform Programmer - Lund, Sweden - Nov 2013 - Apr 2014 </span></Divider>
+          <Divider horizontal>
+            Sony Mobile AB
+            <Responsive as={'span'} minWidth={768}>
+              {' - Platform Programmer - Lund, Sweden - Nov 2013 - Apr 2014'}
+            </Responsive>
+          </Divider>
           <p>
             Worked in the Platform team for Sony’s Firefox OS project.
             Our team was in charge of making the Firefox OS platform compatible with Sony hardware, fixing bugs, ensuring compatibility with Sony branded apps
@@ -227,7 +241,11 @@ class App extends Component {
           <p>
             We worked in a Linux environment, using a variety of programming languages and tools, for example HTML5, JavaScript, Python, C and Git.
           </p>
-          <Divider horizontal>Blackberry - <span>Web Application Developer - Malmö, Sweden - May 2014 - Dec 2014 </span></Divider>
+          <Divider horizontal>Blackberry
+            <Responsive as={'span'} minWidth={768}>
+              {' - Web Application Developer - Malmö, Sweden - May 2014 - Dec 2014'}
+            </Responsive>
+          </Divider>
           <p>
             Worked in a Malmö based Web development team for Blackberry, developing the front end for the Blend application, in close co-operation with several distributed teams in Canada and Russia. Blend is an web based application that displays information from your Blackberry devices on a variety of other devices, like your PC, Mac or Tablet.
             The front end app was built as an AngularJS-app, moving the MVC part of the app to the front end, and everything was being rendered by a QT Webviewer instance.
@@ -235,20 +253,33 @@ class App extends Component {
           <p>
             We worked in Windows and Mac environments, using a variety of programming languages and tools, for example HTML5, JavaScript, AngularJS, Sass and Git.
           </p>
-          <Divider horizontal>Infab Vitamin - <span>Front End Developer - Malmö, Sweden - Jan 2015 - Mar 2015 </span></Divider>
+          <Divider horizontal>
+            Infab Vitamin
+            <Responsive as={'span'} minWidth={768}>
+              {' - Front End Developer - Malmö, Sweden - Jan 2015 - Mar 2015'}
+            </Responsive>
+          </Divider>
           <p>
             Worked with a distributed team in Malmö and India to create a small web application specially ordered by a major US pharmaceutical company.
           </p>
           <p>
             I worked in a Mac environments, using a variety of modern web technologies and libraries, including Less, jQuery, Angular and Bootstrap, and the app was built on a backend server using CakePHP.
           </p>
-          <Divider horizontal>HiQ Karlskrona AB - <span>Programmer - Karlskrona, Sweden - 2010-2013</span></Divider>
+          <Divider horizontal>HiQ Karlskrona AB
+            <Responsive as={'span'} minWidth={768}>
+              {' - Consultant - Karlskrona, Sweden - 2010 - 2013'}
+            </Responsive>
+          </Divider>
           <Header as="h4">Consultancy Assignments</Header>
           <List bulleted>
               <List.Item>Front End Developer, Web Platforms and Applications, Telenor Sverige AB</List.Item>
               <List.Item>Front End Developer, Milou AB </List.Item>
           </List>
-          <Divider horizontal>Telenor Sverige AB - <span>Web Programmer - Karlskrona, Sweden - 2010 - 2013</span></Divider>
+          <Divider horizontal>Telenor Sverige AB
+            <Responsive as={'span'} minWidth={768}>
+              {' - Web Programmer - Karlskrona, Sweden - 2010 - 2013'}
+            </Responsive>
+          </Divider>
           <p>
             Worked with the Telenor “Mina Sidor” team, maintaining and improve a web application used by Telenor’s customers to handle their subscriptions and services.
             The application is built with the Spring MVC and Security frameworks on top of a backend built with Java EE.
@@ -267,14 +298,23 @@ class App extends Component {
             The goal of the project was to allow for special, and more complex, business rules than the ones that are used on the other portals.
           </p>
 
-          <Divider horizontal>Milou AB - <span>Web Programmer - Karlskrona, Sweden - 2013</span></Divider>
+          <Divider horizontal>Milou AB
+            <Responsive as={'span'} minWidth={768}>
+              {' - Web Programmer - Karlskrona, Sweden - 2013'}
+            </Responsive>
+          </Divider>
           <p>
             Worked on a small project to modernize a customer's entire web site, upgrading the client’s CMS and updating it to use modern technologies
             like parallax scrolling and responsive web design, using HTML5 and CSS3.
             The project team was small, and the tempo was hectic, but everything went according to plan and the site was released according to plan.
          </p>
 
-          <Divider horizontal>Blekinge Institute of Tech<span>Project Leader/Developer, Jan 2010 – May 2010</span> </Divider>
+          <Divider horizontal>
+            Blekinge Institute of Technology
+            <Responsive as={'span'} minWidth={768}>
+              {' - Project Leader/Developer, Jan 2010 – May 2010'}
+            </Responsive>
+          </Divider>
           <p>
           Were the project leader and a game logic developer on a big project during my last year at BTH, where we developed our own computer strategy game in C++, using Scrum
            and various other technologies to handle the project management. We completed the project on time, and received overall positive reactions from play testers and graders.
